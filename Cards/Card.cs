49fa48a127f975fb2eefe49cf74a73b5241b31c3
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-03 19:50:15 +0200

    Dodaj pliki projektów.

---
+++ Cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    internal class Card
+    {
+        public string Value { get; set; }
+        public string Suit { get; set; }
+        public string Name { get { return $"{Value} {Suit}"; } }
+
+        public Card(string value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
